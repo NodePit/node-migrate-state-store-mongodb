fd7ed5d4937adac8534b5dc2cc78700d5315ca9d
Dante Calderon <mail@dantecalderon.dev>
2020-08-25 01:28:44 -0500

    Add object options

--- test/index.test.ts
+++ test/index.test.ts
@@ -13,6 +13,7 @@ describe('migrate MongoDB state store', () => {
     lastRun: '1587915479438-my-migration.js'
   };
 
+  const defaultCollectionName = 'migrations';
   const mongoUrl = process.env.MONGO_URL as string;
 
   let client: MongoClient;
@@ -22,7 +23,7 @@ describe('migrate MongoDB state store', () => {
   });
 
   beforeEach(async () => {
-    await client.db().collection(MongoStateStore.collectionName).deleteMany({});
+    await client.db().collection(defaultCollectionName).deleteMany({});
   });
 
   afterAll(async () => {
@@ -50,11 +51,11 @@ describe('migrate MongoDB state store', () => {
     it('throws error when migrations collection contains more than one document', async done => {
       await client
         .db()
-        .collection(MongoStateStore.collectionName)
+        .collection(defaultCollectionName)
         .insertOne({ ...migrationDoc });
       await client
         .db()
-        .collection(MongoStateStore.collectionName)
+        .collection(defaultCollectionName)
         .insertOne({ ...migrationDoc });
       const stateStore = new MongoStateStore(mongoUrl);
       stateStore.load((err, result) => {
@@ -77,7 +78,7 @@ describe('migrate MongoDB state store', () => {
     it('returns migrations document', async done => {
       await client
         .db()
-        .collection(MongoStateStore.collectionName)
+        .collection(defaultCollectionName)
         .insertOne({ ...migrationDoc });
       const stateStore = new MongoStateStore(mongoUrl);
       stateStore.load((err, result) => {
@@ -94,7 +95,7 @@ describe('migrate MongoDB state store', () => {
       stateStore.save(migrationDoc, err => {
         expect(err).toBeUndefined();
         void (async (): Promise<void> => {
-          const docs = await client.db().collection(MongoStateStore.collectionName).find({}).toArray();
+          const docs = await client.db().collection(defaultCollectionName).find({}).toArray();
           expect(docs).toHaveLength(1);
           expect(docs[0]).toMatchObject(migrationDoc);
           done();
@@ -103,12 +104,12 @@ describe('migrate MongoDB state store', () => {
     });
 
     it('replaces existing document in migrations collection', async done => {
-      await client.db().collection(MongoStateStore.collectionName).insertOne({});
+      await client.db().collection(defaultCollectionName).insertOne({});
       const stateStore = new MongoStateStore(mongoUrl);
       stateStore.save(migrationDoc, err => {
         expect(err).toBeUndefined();
         void (async (): Promise<void> => {
-          const docs = await client.db().collection(MongoStateStore.collectionName).find({}).toArray();
+          const docs = await client.db().collection(defaultCollectionName).find({}).toArray();
           expect(docs).toHaveLength(1);
           expect(docs[0]).toMatchObject(migrationDoc);
           done();
